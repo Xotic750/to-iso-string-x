0aebf8c6b8de5afa049d6f9f751a10c4c4f91599
Xotic750 <xotic750@gmail.com>
2017-08-16 01:38:00 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @file Cross-browser toISOString support.
- * @version 1.2.0
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
@@ -9,57 +9,70 @@
 
 'use strict';
 
-var isDate = require('is-date-object');
-var padStart = require('string.prototype.padstart');
-var map = require('array-map-x');
+var nativeToISOString = Date.prototype.toISOString;
 
-var $toISOString = function toISOString(date) {
-  if (isDate(date) === false) {
-    throw new TypeError('toISOString called on incompatible receiver.');
+var $toISOString;
+try {
+  if (nativeToISOString.call(new Date(0)) !== '1970-01-01T00:00:00.000Z') {
+    throw new Error('failed epoch');
   }
 
-  if (isFinite(date) === false || isFinite(date.getTime()) === false) {
-    // Adope Photoshop requires the second check.
-    throw new RangeError('toISOString called on non-finite value.');
-  }
+  $toISOString = function toISOString(date) {
+    return nativeToISOString.call(date);
+  };
+} catch (ignore) {
+  var isDate = require('is-date-object');
+  var padStart = require('string.prototype.padstart');
+  var map = require('array-map-x');
 
-  var year = date.getUTCFullYear();
-  var month = date.getUTCMonth();
-  // see https://github.com/es-shims/es5-shim/issues/111
-  year += Math.floor(month / 12);
-  month = ((month % 12) + 12) % 12;
+  $toISOString = function toISOString(date) {
+    if (isDate(date) === false) {
+      throw new TypeError('toISOString called on incompatible receiver.');
+    }
 
-  // the date time string format is specified in 15.9.1.15.
-  var parts = [
-    month + 1,
-    date.getUTCDate(),
-    date.getUTCHours(),
-    date.getUTCMinutes(),
-    date.getUTCSeconds()
-  ];
+    if (isFinite(date) === false || isFinite(date.getTime()) === false) {
+      // Adope Photoshop requires the second check.
+      throw new RangeError('toISOString called on non-finite value.');
+    }
 
-  var sign;
-  if (year < 0) {
-    sign = '-';
-  } else if (year > 9999) {
-    sign = '+';
-  } else {
-    sign = '';
-  }
+    var year = date.getUTCFullYear();
+    var month = date.getUTCMonth();
+    // see https://github.com/es-shims/es5-shim/issues/111
+    year += Math.floor(month / 12);
+    month = ((month % 12) + 12) % 12;
 
-  year = sign + padStart(Math.abs(year), sign ? 6 : 4, '0');
-  var result = map(parts, function _for(item) {
-    // pad months, days, hours, minutes, and seconds to have two digits.
-    return padStart(item, 2, '0');
-  });
+    // the date time string format is specified in 15.9.1.15.
+    var parts = [
+      month + 1,
+      date.getUTCDate(),
+      date.getUTCHours(),
+      date.getUTCMinutes(),
+      date.getUTCSeconds()
+    ];
 
-  var dateStr = year + '-' + result.slice(0, 2).join('-');
-  // pad milliseconds to have three digits.
-  var msStr = padStart(date.getUTCMilliseconds(date), 3, '0');
-  var timeStr = result.slice(2).join(':') + '.' + msStr;
+    var sign;
+    if (year < 0) {
+      sign = '-';
+    } else if (year > 9999) {
+      sign = '+';
+    } else {
+      sign = '';
+    }
 
-  return dateStr + 'T' + timeStr + 'Z';
-};
+    year = sign + padStart(Math.abs(year), sign ? 6 : 4, '0');
+    var result = map(parts, function _mapper(item) {
+      // pad months, days, hours, minutes, and seconds to have two digits.
+      return padStart(item, 2, '0');
+    });
+
+    var dateStr = year + '-' + result.slice(0, 2).join('-');
+    // pad milliseconds to have three digits.
+    var msStr = padStart(date.getUTCMilliseconds(date), 3, '0');
+    var timeStr = result.slice(2).join(':') + '.' + msStr;
+
+    return dateStr + 'T' + timeStr + 'Z';
+  };
+}
 
 /**
  * This method returns a string in simplified extended ISO format (ISO 8601),
