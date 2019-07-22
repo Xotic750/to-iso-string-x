e2e63c038776771836f6c62c009bb72d33be15bc
Graham Fairweather <xotic750@gmail.com>
2019-07-22 15:27:43 +0200

    :bookmark: v2.0.5
    Dependency updates

--- dist/to-iso-string-x.js
+++ dist/to-iso-string-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-21T23:26:31.281Z",
+  "date": "2019-07-22T13:27:37.378Z",
   "describe": "",
   "description": "Cross-browser toISOString support.",
   "file": "to-iso-string-x.js",
   "hash": "ecc4ebdd0f125a966522",
   "license": "MIT",
-  "version": "2.0.4"
+  "version": "2.0.5"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
