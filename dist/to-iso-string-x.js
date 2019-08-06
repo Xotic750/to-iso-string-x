1f189c7dd08288f2d704beb41fb9ae5346a78d8f
Graham Fairweather <xotic750@gmail.com>
2019-08-06 03:17:31 +0200

    :bookmark: v2.0.15
    Coveralls

--- dist/to-iso-string-x.js
+++ dist/to-iso-string-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-05T21:46:50.403Z",
+  "date": "2019-08-06T01:17:25.542Z",
   "describe": "",
   "description": "Cross-browser toISOString support.",
   "file": "to-iso-string-x.js",
